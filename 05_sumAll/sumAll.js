de5c1fdf19f385be3f16a4f1a4ed4514115e7dbf
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-05-18 14:09:44 -0700

    do exercises up to 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!(num1 <= 0) && !(num2 <= 0) && Number.isInteger(num1) && Number.isInteger(num2)) {
 
-};
+        let sum = 0,
+        numArr = [num1, num2].sort(function(a, b){return a - b});
+
+        for (let i = numArr[0]; i <= numArr[1]; i++) {
+            sum += i;
+        };
+    
+        return sum;
+    }
+    console.log('ERROR');
+    return 'ERROR'
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Both #s need to be positive, if not, return 'ERROR'
+// '    '    '   '  '  real numbers '   '     '
+// Sort numbers so it calculates smaller number first
+// initialize loop with starting number, stop at highest number
+// add each number to a total variable
